b1dc3709d910963b82f2c09ebc21d064cf921452
Thomas-Fch <thomas.fachinetti@gmail.com>
2023-06-15 16:43:26 +0200

    preject initialize, package node: prettier, eslint ,react routedom, sass

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { Route, Routes } from "react-router-dom";
+import "./sass/style.scss";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <Routes>
+    </Routes> */}
+    </>
+  );
+}
+
+export default App;
