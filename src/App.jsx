31e931da6f54e04340f726f5a524a1034742661e
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-07-11 14:08:20 +0200

    correctif login redirection privateroute

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,29 @@ import {
   Route,
   Routes,
   Navigate,
+  useLocation,
 } from "react-router-dom";
 import "./sass/style.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/login/Login";
 import Home from "./pages/home";
 import { useSelector, useDispatch } from "react-redux";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getCurrentUser } from "./services/users";
 import { signin } from "./store/auth";
 
 const App = () => {
   const dispatch = useDispatch();
-
+  const [visible, setVisible] = useState(false);
   const reloadStore = async () => {
     try {
       const result = await getCurrentUser();
-      dispatch(signin(result.data));
+      console.log("result", result);
+      dispatch(signin(result.data, { isLogged: true }));
+      setVisible(true);
     } catch (error) {
       console.error(error);
+      setVisible(true);
     }
   };
 
@@ -29,7 +33,7 @@ const App = () => {
     reloadStore();
   }, []);
 
-  return (
+  return visible ? (
     <Router>
       <div>
         <Routes>
@@ -54,6 +58,8 @@ const App = () => {
         </Routes>
       </div>
     </Router>
+  ) : (
+    ""
   );
 };
 
@@ -62,10 +68,14 @@ const admins = [
   1, //  => Admin
 ];
 
-const PrivateRoute = ({ children, admin = 0 }) => {
+const PrivateRoute = ({ children, admin = 1 }) => {
   const auth = useSelector((state) => state.auth);
+  const location = useLocation();
+  console.log("location", location.pathname);
+  console.log(auth);
+  console.log("auth.isLogged", auth.isLogged);
   if (auth.isLogged) {
-    console.log("auth.user", auth.user);
+    console.log("auth.user new", auth);
     if (
       auth.user?.admin == admin ||
       admins.indexOf(auth.user?.admin) >= admins.indexOf(admin)
