e7923d36f7795e9bf3f20e1441a6059c7baaf853
Thomas-Fch <thomas.fachinetti@gmail.com>
2023-07-11 17:01:56 +0200

    squelette front carte projet terminé

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,33 @@
+import Avatar from "../../assets/default_user.png";
+import Background from "../../assets/default_background_project.jpg";
+
+export default function ProjectCard() {
+  return (
+    <figure>
+      <img src={Background} className="backgroundProject" alt="projet" />
+      <figcaption>
+        <h3 className="c-blue ">Mon super Titre</h3>
+        <div className="tagsProject">
+          <p className="tag-blue">tag n°1</p>
+          <p className="tag-red">tag n°2</p>
+        </div>
+        <div className="userInfosDate">
+          <div className="userProjectInfos">
+            <img
+              src={Avatar}
+              alt="profil utilisateur"
+              className="avatarProject"
+            />
+            <p className="c-blue ">
+              par<span className="c-blue "> Prénom Nom</span>
+            </p>
+          </div>
+          <div className="calendar">
+            {/* <div className="calendarColor" /> */}
+            <strong>7</strong>
+          </div>
+        </div>
+      </figcaption>
+    </figure>
+  );
+}
