d0bca5b285c37f2608f04e8ab97fcd8602ea7303
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-07-16 21:11:14 +0200

    fix service api post

--- src/services/post.js
+++ src/services/post.js
@@ -1,11 +1,11 @@
 import api from "./api";
 
 const getAllPost = () => {
-  api.get("/api/posts");
+  return api.get("/api/posts");
 };
 
 const getCurrentPost = (id) => {
-  api.get(`/api/posts/${id}`);
+  return api.get(`/api/posts/${id}`);
 };
 
 export { getAllPost, getCurrentPost };
