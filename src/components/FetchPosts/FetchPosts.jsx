2fd8e01776a01ecbe5f6ff6188593f24e8e0557f
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-07-16 21:04:09 +0200

    sync admin branch local

--- src/components/FetchPosts/FetchPosts.jsx
+++ src/components/FetchPosts/FetchPosts.jsx
@@ -2,21 +2,24 @@ import { useEffect, useState } from "react";
 import api from "../../services/api";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import Pagination from "../Pagination/Pagination";
-
+import { getAllPosts } from "../../services/post";
 export default function FetchPosts() {
   const [posts, setPosts] = useState([]);
   const [currentItems, setCurrentItems] = useState([]);
 
+  const searchData = async () => {
+    try {
+      const postsData = await getAllPosts();
+      setPosts(postsData.data);
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
   useEffect(() => {
-    api
-      .get("/api/posts")
-      .then((res) => {
-        setPosts(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    searchData();
   }, []);
+
   return (
     <>
       <ul className="fetchPosts">
--- src/services/post.js
+++ src/services/post.js
@@ -1,7 +1,7 @@
 import api from "./api";
 
-const getAllRoles = async (req, res) => {
-  return api.get("/api/roles")
+const getAllPosts = async (req, res) => {
+  return api.get("/api/posts")
 }
 
-export { getAllRoles };
+export { getAllPosts };
