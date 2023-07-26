0ea7e54b09abf74870a29d2ea7976edcad56e28b
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-07-26 23:11:26 +0200

    avis delete/edit/add corriger et fini

--- src/pages/projectViewById/CreateAvis.jsx
+++ src/pages/projectViewById/CreateAvis.jsx
@@ -1,3 +1,63 @@
+// import { useState } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { addAvis } from "../../services/avis";
+// import { createAvis } from "../../store/avis";
+
+// export default function CreateAvis({ post }) {
+//   const [actif, setActif] = useState(false);
+//   const [text, setText] = useState("");
+//   const dispatch = useDispatch();
+//   const auth = useSelector((state) => state.auth);
+//   const avis = useSelector((state) => state.avis);
+
+//   const handleActif = () => {
+//     setActif(!actif);
+//   };
+//   const sendText = async () => {
+//     try {
+//       const avisData = { user_id: auth.user.id, post_id: post.id, text: text };
+//       const addNewAvis = await addAvis(avisData);
+//       const newDataAvis = {
+//         date: addNewAvis.data.date,
+//         firstname: addNewAvis.data.user[0].firstname,
+//         lastname: addNewAvis.data.user[0].lastname,
+//         photo: addNewAvis.data.user[0].avatar,
+//         user_id: auth.user.id,
+//         id: addNewAvis.data.addAlertByUser.id,
+//         post_id: post.id,
+//         text: text,
+//       };
+//       dispatch(createAvis(newDataAvis));
+//       setActif(false);
+//       setText("");
+//     } catch (err) {
+//       console.error("err", err);
+//     }
+//   };
+//   return (
+//     <div>
+//       <button type="button" onClick={handleActif} className="createAvis">
+//         create
+//       </button>
+//       {actif ? (
+//         <>
+//           <input
+//             className="createAvisText"
+//             type="
+//           text"
+//             placeholder="Rédigez votre avis ici."
+//             onChange={(e) => setText(e.target.value)}
+//             defaultValue={text}
+//           />
+//           <button type="button" className="createAvis" onClick={sendText}>
+//             Envoyer
+//           </button>
+//         </>
+//       ) : null}
+//     </div>
+//   );
+// }
+
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addAvis } from "../../services/avis";
@@ -13,6 +73,7 @@ export default function CreateAvis({ post }) {
   const handleActif = () => {
     setActif(!actif);
   };
+
   const sendText = async () => {
     try {
       const avisData = { user_id: auth.user.id, post_id: post.id, text: text };
@@ -23,7 +84,7 @@ export default function CreateAvis({ post }) {
         lastname: addNewAvis.data.user[0].lastname,
         photo: addNewAvis.data.user[0].avatar,
         user_id: auth.user.id,
-        id: addNewAvis.data.addAlertByUser.id,
+        id: addNewAvis.data.addAvisByUser.id,
         post_id: post.id,
         text: text,
       };
@@ -34,6 +95,7 @@ export default function CreateAvis({ post }) {
       console.error("err", err);
     }
   };
+
   return (
     <div>
       <button type="button" onClick={handleActif} className="createAvis">
@@ -43,11 +105,10 @@ export default function CreateAvis({ post }) {
         <>
           <input
             className="createAvisText"
-            type="
-          text"
+            type="text"
             placeholder="Rédigez votre avis ici."
             onChange={(e) => setText(e.target.value)}
-            defaultValue={text}
+            value={text}
           />
           <button type="button" className="createAvis" onClick={sendText}>
             Envoyer
--- src/pages/projectViewById/ProjectAvis.jsx
+++ src/pages/projectViewById/ProjectAvis.jsx
@@ -1,343 +1,3 @@
-// import PropTypes from "prop-types";
-// import { useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import CreateAvis from "./CreateAvis";
-// import { removeAvis } from "../../store/avis";
-// import { deleteAvis, editAvis } from "../../services/avis";
-
-// export default function ProjectAvis({ avis, post }) {
-//   const [visibleModal, setvisibleModal] = useState(false);
-//   const [errMessage, setErrMessage] = useState("");
-//   const [avisId, setAvisId] = useState(0);
-//   const [editingIndex, setEditingIndex] = useState(null);
-//   const [editedText, setEditedText] = useState("");
-
-//   const dispatch = useDispatch();
-//   const avisRedux = useSelector((state) => state.avis);
-//   const auth = useSelector((state) => state.auth);
-
-//   const handleClickDelete = (avis_id) => {
-//     setvisibleModal(!visibleModal);
-//     setAvisId(avis_id);
-//   };
-
-//   const handleInputChange = (e) => {
-//     setEditedText(e.target.value);
-//   };
-
-//   const changeEDit = (text) => {};
-
-//   const handleClickCancel = () => {
-//     setvisibleModal(!visibleModal);
-//     setAvisId(0);
-//   };
-
-//   const cancelEdit = (index, text) => {
-//     setEditingIndex(index);
-//     setEditedText(text);
-//   };
-
-//   const validEdit = async (id) => {
-//     console.log("id", id);
-//     // try {
-//     //   await editAvis(id);
-//     //   setEdit(false);
-//     // } catch (err) {
-//     //   console.error("err", err);
-//     // }
-//   };
-
-//   const handleClickFetch = async () => {
-//     try {
-//       await deleteAvis(avisId);
-//       dispatch(removeAvis(avisId));
-//       setvisibleModal(!visibleModal);
-//     } catch (err) {
-//       console.error("err", err);
-//       setErrMessage(
-//         "Nous rencontrons un problème. Veuillez réessayer plus tard."
-//       );
-//     }
-//   };
-
-//   return (
-//     <div id="container">
-//       <CreateAvis post={post} />
-//       <div className="avisContainer">
-//         {avis.map((data, index) => (
-//           <div className="avisByUser" key={data.date}>
-//             {auth.user.admin === 1 || auth.user.id === data.user_id ? (
-//               <>
-//                 <span
-//                   className="action_edit_admin_user"
-//                   title="Modifier"
-//                   onClick={() => changeEDit(index, data.text)}
-//                 >
-//                   &#x270E;
-//                 </span>
-//                 <span
-//                   className="action_delete_admin_user"
-//                   title="Supprimer"
-//                   onClick={() => handleClickDelete(data.id)}
-//                 >
-//                   &#x2716;
-//                 </span>
-//               </>
-//             ) : null}
-//             <div className="userInfos">
-//               <div className="userProjectInfos">
-//                 <img
-//                   src={data.photo}
-//                   alt="profil utilisateur"
-//                   className="avatarProject"
-//                 />
-//                 <p className="c-blue">
-//                   <span className="c-blue ">
-//                     {data.firstname} {data.lastname}
-//                   </span>
-//                   - le {data.date.slice(0, 10)}
-//                 </p>
-//               </div>
-//             </div>
-//             {editingIndex === index ? (
-//               <>
-//                 <input
-//                   type="text"
-//                   // onChange={(e) => setText(e.target.value)}
-//                   onChange={(e) => handleInputChange(e)}
-//                   value={text}
-//                   className="c-blue avisByUserText"
-//                 />
-//                 <button
-//                   type="button"
-//                   className="createAvis"
-//                   onClick={() => cancelEdit(data.text)}
-//                 >
-//                   Annuler
-//                 </button>
-//                 <button
-//                   type="button"
-//                   className="createAvis"
-//                   onClick={() => validEdit(data.id)}
-//                 >
-//                   Confirmer
-//                 </button>
-//               </>
-//             ) : (
-//               <h1 className="c-blue avisByUserText">{data.text}</h1>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//       {visibleModal ? (
-//         <div id="modal_delete" className="modal">
-//           <div className="modal_content">
-//             {errMessage && <p className="p_error_modal">{errMessage}</p>}
-//             <h2>Confirmation de suppression</h2>
-//             <p>Voulez-vous vraiment cette avis ?</p>
-//             <div className="modal_buttons">
-//               <button type="button" id="btn_cancel" onClick={handleClickCancel}>
-//                 Annuler
-//               </button>
-//               <button type="button" id="btn_confirm" onClick={handleClickFetch}>
-//                 Confirmer
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       ) : (
-//         ""
-//       )}
-//     </div>
-//   );
-// }
-
-// ProjectAvis.propTypes = {
-//   avis: PropTypes.shape({
-//     firstname: PropTypes.string,
-//     lastname: PropTypes.string,
-//     photo: PropTypes.string,
-//     text: PropTypes.string,
-//     map: PropTypes.func,
-//   }).isRequired,
-// };
-
-// import PropTypes from "prop-types";
-// import { useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import CreateAvis from "./CreateAvis";
-// import { removeAvis, editAvisData } from "../../store/avis";
-// import { deleteAvis, editAvis } from "../../services/avis";
-
-// export default function ProjectAvis({ avis, post }) {
-//   const [visibleModal, setvisibleModal] = useState(false);
-//   const [errMessage, setErrMessage] = useState("");
-//   const [avisId, setAvisId] = useState(0);
-//   const [editingIndex, setEditingIndex] = useState(null);
-//   const [editedText, setEditedText] = useState("");
-
-//   const dispatch = useDispatch();
-//   const avisRedux = useSelector((state) => state.avis);
-//   const auth = useSelector((state) => state.auth);
-
-//   const handleClickDelete = (id_avis) => {
-//     setvisibleModal(!visibleModal);
-//     console.log("id_avis");
-//     setAvisId(id_avis);
-//   };
-
-//   const handleInputChange = (e) => {
-//     setEditedText(e.target.value);
-//   };
-
-//   const changeEdit = (index, text) => {
-//     setEditingIndex(index);
-//     setEditedText(text);
-//   };
-
-//   const handleClickCancel = () => {
-//     setvisibleModal(!visibleModal);
-//     setAvisId(0);
-//   };
-
-//   const cancelEdit = (index, text) => {
-//     setEditingIndex(null);
-//     setEditedText("");
-//   };
-
-//   const validEdit = async (id) => {
-//     console.log("id", id);
-//     try {
-//       await editAvis(id, { text: editedText });
-//       dispatch(editAvisData({ id, newText: editedText }));
-//       setEditedText("");
-//       setEditingIndex(null);
-//     } catch (err) {
-//       console.error("err", err);
-//     }
-//   };
-
-//   const handleClickFetch = async () => {
-//     try {
-//       console.log("avisId", avisId);
-//       await deleteAvis(avisId);
-//       dispatch(removeAvis(avisId));
-//       setvisibleModal(!visibleModal);
-//     } catch (err) {
-//       console.error("err", err);
-//       setErrMessage(
-//         "Nous rencontrons un problème. Veuillez réessayer plus tard."
-//       );
-//     }
-//   };
-
-//   return (
-//     <div id="container">
-//       <CreateAvis post={post} />
-//       <div className="avisContainer">
-//         {avis.map((data, index) => (
-//           <div className="avisByUser" key={data.date}>
-//             {auth.user.admin === 1 || auth.user.id === data.user_id ? (
-//               <>
-//                 <span
-//                   className="action_edit_admin_user"
-//                   title="Modifier"
-//                   onClick={() => changeEdit(index, data.text)}
-//                 >
-//                   &#x270E;
-//                 </span>
-//                 <span
-//                   className="action_delete_admin_user"
-//                   title="Supprimer"
-//                   onClick={() => handleClickDelete(data.id)}
-//                 >
-//                   &#x2716;
-//                 </span>
-//               </>
-//             ) : null}
-//             <div className="userInfos">
-//               <div className="userProjectInfos">
-//                 <img
-//                   src={data.photo}
-//                   alt="profil utilisateur"
-//                   className="avatarProject"
-//                 />
-//                 <p className="c-blue">
-//                   <span className="c-blue ">
-//                     {data.firstname} {data.lastname}
-//                   </span>
-//                   - le {data.date.slice(0, 10)}
-//                 </p>
-//               </div>
-//             </div>
-//             {editingIndex === index ? (
-//               <>
-//                 <input
-//                   type="text"
-//                   onChange={handleInputChange}
-//                   value={editedText}
-//                   className="c-blue avisByUserText"
-//                 />
-//                 <button
-//                   type="button"
-//                   className="createAvis"
-//                   onClick={() => cancelEdit(index, data.text)}
-//                 >
-//                   Annuler
-//                 </button>
-//                 <button
-//                   type="button"
-//                   className="createAvis"
-//                   onClick={() => validEdit(data.id)}
-//                 >
-//                   Confirmer
-//                 </button>
-//               </>
-//             ) : (
-//               <h1
-//                 className="c-blue avisByUserText"
-//                 onClick={() => changeEdit(index, data.text)}
-//               >
-//                 {data.text}
-//               </h1>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//       {visibleModal ? (
-//         <div id="modal_delete" className="modal">
-//           <div className="modal_content">
-//             {errMessage && <p className="p_error_modal">{errMessage}</p>}
-//             <h2>Confirmation de suppression</h2>
-//             <p>Voulez-vous vraiment cette avis ?</p>
-//             <div className="modal_buttons">
-//               <button type="button" id="btn_cancel" onClick={handleClickCancel}>
-//                 Annuler
-//               </button>
-//               <button type="button" id="btn_confirm" onClick={handleClickFetch}>
-//                 Confirmer
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       ) : (
-//         ""
-//       )}
-//     </div>
-//   );
-// }
-
-// ProjectAvis.propTypes = {
-//   avis: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       firstname: PropTypes.string,
-//       lastname: PropTypes.string,
-//       photo: PropTypes.string,
-//       text: PropTypes.string,
-//     })
-//   ).isRequired,
-// };
-
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -348,17 +8,33 @@ import { deleteAvis, editAvis } from "../../services/avis";
 export default function ProjectAvis({ avis, post }) {
   const [visibleModal, setvisibleModal] = useState(false);
   const [errMessage, setErrMessage] = useState("");
-  const [avisId, setAvisId] = useState(0);
   const [editingIndex, setEditingIndex] = useState(null);
   const [editedText, setEditedText] = useState("");
+  const [avisIdDelete, setAvisIdDelete] = useState(0);
 
   const dispatch = useDispatch();
-  const avisRedux = useSelector((state) => state.avis);
   const auth = useSelector((state) => state.auth);
 
-  const handleClickDelete = (avisToDelete) => {
+  const handleModalDelter = async (avis_id) => {
+    setAvisIdDelete(avis_id);
     setvisibleModal(!visibleModal);
-    setAvisId(avisToDelete.id);
+  };
+
+  const handleClickDelete = async () => {
+    try {
+      const avisToDelete = avis.find((avis) => avis.id === avisIdDelete);
+      if (avisToDelete) {
+        await deleteAvis(avisIdDelete);
+        dispatch(removeAvis(avisIdDelete));
+        setvisibleModal(!visibleModal);
+        setErrMessage("");
+      }
+    } catch (err) {
+      console.error("err", err);
+      setErrMessage(
+        "Nous rencontrons un problème. Veuillez réessayer plus tard."
+      );
+    }
   };
 
   const handleInputChange = (e) => {
@@ -372,10 +48,9 @@ export default function ProjectAvis({ avis, post }) {
 
   const handleClickCancel = () => {
     setvisibleModal(!visibleModal);
-    setAvisId(0);
   };
 
-  const cancelEdit = (index, text) => {
+  const cancelEdit = () => {
     setEditingIndex(null);
     setEditedText("");
   };
@@ -389,23 +64,6 @@ export default function ProjectAvis({ avis, post }) {
       setEditingIndex(null);
     } catch (err) {
       console.error("err", err);
-    }
-  };
-
-  const handleClickFetch = async () => {
-    try {
-      // Vérifier si l'avis à supprimer existe toujours dans le state Redux
-      const avisToDelete = avisRedux.find((avis) => avis.id === avisId);
-      if (avisToDelete) {
-        await deleteAvis(avisId);
-        dispatch(removeAvis(avisId));
-      }
-      setvisibleModal(!visibleModal);
-    } catch (err) {
-      console.error("err", err);
-      setErrMessage(
-        "Nous rencontrons un problème. Veuillez réessayer plus tard."
-      );
     }
   };
 
@@ -427,7 +85,7 @@ export default function ProjectAvis({ avis, post }) {
                 <span
                   className="action_delete_admin_user"
                   title="Supprimer"
-                  onClick={() => handleClickDelete(data)}
+                  onClick={() => handleModalDelter(data.id)}
                 >
                   &#x2716;
                 </span>
@@ -459,7 +117,7 @@ export default function ProjectAvis({ avis, post }) {
                 <button
                   type="button"
                   className="createAvis"
-                  onClick={() => cancelEdit(index, data.text)}
+                  onClick={cancelEdit}
                 >
                   Annuler
                 </button>
@@ -472,12 +130,12 @@ export default function ProjectAvis({ avis, post }) {
                 </button>
               </>
             ) : (
-              <h1
+              <p
                 className="c-blue avisByUserText"
                 onClick={() => changeEdit(index, data.text)}
               >
                 {data.text}
-              </h1>
+              </p>
             )}
           </div>
         ))}
@@ -487,20 +145,39 @@ export default function ProjectAvis({ avis, post }) {
           <div className="modal_content">
             {errMessage && <p className="p_error_modal">{errMessage}</p>}
             <h2>Confirmation de suppression</h2>
-            <p>Voulez-vous vraiment cette avis ?</p>
+            <p>Voulez-vous vraiment supprimer cet avis ?</p>
             <div className="modal_buttons">
               <button type="button" id="btn_cancel" onClick={handleClickCancel}>
                 Annuler
               </button>
-              <button type="button" id="btn_confirm" onClick={handleClickFetch}>
+              <button
+                type="button"
+                id="btn_confirm"
+                onClick={handleClickDelete}
+              >
                 Confirmer
               </button>
             </div>
           </div>
         </div>
-      ) : (
-        ""
-      )}
+      ) : null}
     </div>
   );
 }
+
+ProjectAvis.propTypes = {
+  avis: PropTypes.arrayOf(
+    PropTypes.shape({
+      firstname: PropTypes.string,
+      lastname: PropTypes.string,
+      photo: PropTypes.string,
+      text: PropTypes.string,
+      user_id: PropTypes.number,
+      id: PropTypes.number,
+      date: PropTypes.string,
+    })
+  ).isRequired,
+  post: PropTypes.shape({
+    id: PropTypes.number,
+  }).isRequired,
+};
