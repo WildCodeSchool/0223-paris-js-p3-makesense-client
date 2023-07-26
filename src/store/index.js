27a44a399ca5bc825a7b1aa01876d5bc03f4e414
etienne chamarier <etienne.chamarier@gmail.com>
2023-07-26 17:52:41 +0200

    maj css

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./auth";
-<<<<<<< HEAD
-import users from "./users"
-import project from "./projectSlice"
-
-export default configureStore({
-  reducer: { auth, users, project },
-=======
 import users from "./users";
 import jobs from "./jobs";
 import roles from "./roles";
@@ -15,5 +8,4 @@ import avis from "./avis";
 
 export default configureStore({
   reducer: { auth, users, jobs, roles, posts, avis },
->>>>>>> ff4aaf0e7bdbf0cb041fe7229805aa94e40d1efe
 });
