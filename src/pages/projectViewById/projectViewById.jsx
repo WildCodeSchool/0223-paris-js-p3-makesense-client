1e97caa9044202d37212adcbfd761b8804002826
etienne chamarier <etienne.chamarier@gmail.com>
2023-07-26 15:38:00 +0200

    add setting

--- src/pages/projectViewById/projectViewById.jsx
+++ src/pages/projectViewById/projectViewById.jsx
@@ -1,11 +1,27 @@
 import ProjectResume from "./ProjectResume";
 import Timeline from "../../components/Timeline/Timeline"
 
+const stepsData = [
+  { date: "2023-05-25" },
+  { date: "2023-05-27" },
+  { date: "2023-05-19" },
+  { date: "2023-07-26" },
+  { date: "2023-07-28" },
+  // { name: "1", date: "2023-05-25" },
+  // { name: "2", date: "2023-05-27" },
+  // { name: "3", date: "2023-05-19" },
+  // { name: "4", date: "2023-07-26" },
+  // { name: "5", date: "2023-07-28" },
+];
+
+
 export default function ProjectViewById() {
   return (
-    <section id="projectViewById">
-      {/* <ProjectResume /> */}
-      <Timeline/>
-    </section>
+    // <section id="projectViewById">
+    //   {/* <ProjectResume /> */}
+    <div>
+      <Timeline steps={stepsData} />
+      </div>
+    // </section>
   );
 }
